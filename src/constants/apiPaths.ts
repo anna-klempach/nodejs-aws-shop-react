9dcbf21fbed07200b2fcde14d2871c24ad02ef1b
Hanna Klempach <hklempach@wideorbit.com>
2023-06-01 13:19:55 +0200

    task-3 / Integrate products endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,7 @@
+const BASE_URL = "https://hzrr35n5la.execute-api.us-east-1.amazonaws.com/prod";
+
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  products: `${BASE_URL}/products`,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
